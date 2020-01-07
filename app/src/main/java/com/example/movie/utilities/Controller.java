43fc7277bc0509e415d32ac12ed7b75b775f9040
MUTAI <yegoallen@gmail.com>
2020-01-07 19:55:16 +0300

    created singeltonof retrofit

--- app/src/main/java/com/example/movie/utilities/Controller.java
+++ app/src/main/java/com/example/movie/utilities/Controller.java
@@ -10,5 +10,20 @@ import static com.example.movie.utilities.Constant.MOVIE_BASE_URL;
  */
 public class Controller {
 
-    /
+    /** Static variable for Retrofit */
+    private static Retrofit sRetrofit = null;
+
+    public static Retrofit getClient() {
+        if (sRetrofit == null) {
+            // Create the Retrofit instance using the builder
+            sRetrofit = new Retrofit.Builder()
+                    // Set the API base URL
+                    .baseUrl(MOVIE_BASE_URL)
+                    // Use GsonConverterFactory class to generate an implementation of the TheMovieApi interface
+                    // which uses Gson for its deserialization
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return sRetrofit;
+    }
 }
