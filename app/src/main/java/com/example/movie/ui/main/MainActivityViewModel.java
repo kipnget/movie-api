8f1389b2213d8094b022f2237cd1dfc1204e3b7c
MUTAI <yegoallen@gmail.com>
2020-01-07 20:39:46 +0300

    model view for main activity

---
+++ app/src/main/java/com/example/movie/ui/main/MainActivityViewModel.java
@@ -0,0 +1,66 @@
+package com.example.movie.ui.main;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
+
+import com.example.movie.data.MovieDataSourceFactory;
+import com.example.movie.data.MovieEntry;
+import com.example.movie.data.MovieRepository;
+import com.example.movie.model.Movie;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import static com.example.movie.utilities.Constant.INITIAL_LOAD_SIZE_HINT;
+import static com.example.movie.utilities.Constant.NUMBER_OF_FIXED_THREADS_FIVE;
+import static com.example.movie.utilities.Constant.PAGE_SIZE;
+import static com.example.movie.utilities.Constant.PREFETCH_DISTANCE;
+
+/**
+ * {@link ViewModel} for MainActivity
+ */
+public class MainActivityViewModel extends ViewModel {
+
+    private final MovieRepository mRepository;
+
+    private LiveData<PagedList<Movie>> mMoviePagedList;
+    private LiveData<List<MovieEntry>> mFavoriteMovies;
+    private String mSortCriteria;
+
+    public MainActivityViewModel(MovieRepository repository, String sortCriteria) {
+        mRepository = repository;
+        mSortCriteria = sortCriteria;
+        init(sortCriteria);
+    }
+
+    /**
+     * Initialize the paged list
+     */
+    private void init(String sortCriteria) {
+        Executor executor = Executors.newFixedThreadPool(NUMBER_OF_FIXED_THREADS_FIVE);
+
+        // Create a MovieDataSourceFactory providing DataSource generations
+        MovieDataSourceFactory movieDataFactory = new MovieDataSourceFactory(sortCriteria);
+
+        // Configures how a PagedList loads content from the MovieDataSource
+        PagedList.Config config = (new PagedList.Config.Builder())
+                .setEnablePlaceholders(false)
+                // Size hint for initial load of PagedList
+                .setInitialLoadSizeHint(INITIAL_LOAD_SIZE_HINT)
+                // Size of each page loaded by the PagedList
+                .setPageSize(PAGE_SIZE)
+                // Prefetch distance which defines how far ahead to load
+                .setPrefetchDistance(PREFETCH_DISTANCE)
+                .build();
+
+        // The LivePagedListBuilder class is used to get a LiveData object of type PagedList
+        mMoviePagedList = new LivePagedListBuilder<>(movieDataFactory, config)
+                .setFetchExecutor(executor)
+                .build();
+    }
+
+
+}
