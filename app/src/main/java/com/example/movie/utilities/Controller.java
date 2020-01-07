6cf207e7caa0593f7c923dc1521d818a1f35e61b
MUTAI <yegoallen@gmail.com>
2020-01-07 19:51:27 +0300

    created class controller

---
+++ app/src/main/java/com/example/movie/utilities/Controller.java
@@ -0,0 +1,14 @@
+package com.example.movie.utilities;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.example.movie.utilities.Constant.MOVIE_BASE_URL;
+
+/**
+ *  Create a singleton of Retrofit.
+ */
+public class Controller {
+
+    /
+}
