146a2f2ca09cb40300be96e0452f76753f771e53
MUTAI <yegoallen@gmail.com>
2020-01-08 10:29:11 +0300

    string sort criteria and viewmodel for movie repo

---
+++ app/src/main/java/com/example/movie/ui/main/MainViewModelFactory.java
@@ -0,0 +1,27 @@
+package com.example.movie.ui.main;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.movie.data.MovieRepository;
+
+/**
+ * Factory method that allows us to create a ViewModel with a constructor that takes a
+ * {@link MovieRepository} and String sortCriteria
+ */
+public class MainViewModelFactory extends ViewModelProvider.NewInstanceFactory {
+
+    private final MovieRepository mRepository;
+    private final String mSortCriteria;
+
+    public MainViewModelFactory(MovieRepository repository, String sortCriteria) {
+        this.mRepository = repository;
+        this.mSortCriteria = sortCriteria;
+    }
+
+    @Override
+    public <T extends ViewModel> T create(Class<T> modelClass) {
+        //noinspection unchecked
+        return (T) new MainActivityViewModel(mRepository, mSortCriteria);
+    }
+}
